b09f97bd16a7b6b1614ce6231e88e3adfc723831
Augusta MacDonald <optissimum@gmail.com>
2020-04-06 16:42:27 -0400

    Updated with a button that generates more items on the page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      previous_gen: {},
-      current_gen: null
+      generated: []
     };
   }
 
   random(range) {
-    return Math.floor(Math.random() * range)
+    return Math.floor(Math.random() * range);
   }
 
-  render() {
+  updateList(item){
+    console.log(item);
+    this.setState({generated: [...this.state.generated, {"weapon": item.weapon, "property": item.property}]});
+    console.log(this.state);
+  }
+
+  generateItem(){
+    let item = {}
     let property = Lists
       .property[this.random(Lists.property.length)]
       .replace("#trigger", Lists.trigger[this.random(Lists.trigger.length)])
@@ -26,24 +32,35 @@ class App extends Component {
       .replace("#damage_type", Lists.damage_type[this.random(Lists.damage_type.length)])
       .replace("#action", Lists.action[this.random(Lists.action.length)])
       .replace("#save", Lists.save[this.random(Lists.save.length)])
-      .replace("#distance", Lists.distance[this.random(Lists.distance.length)])
-      .replace("#distance", Lists.distance[this.random(Lists.distance.length)])
+      .replace("#distance", Lists.distance[this.random(Lists.distance.length)] + "ft")
+      .replace("#distance", Math.min([Lists.distance[this.random(Lists.distance.length)], Lists.distance[this.random(Lists.distance.length)]]) + "ft")
       .replace("#duration", Lists.duration[this.random(Lists.duration.length)]);
     property = property
       .charAt(0)
-      .toUpperCase() + property.substr(1) + ".";
-    let item = Lists.weapons[this.random(Lists.weapons.length)];
+      .toUpperCase() + property.substr(1);
+    let weapon = Lists.weapons[this.random(Lists.weapons.length)];
+    item = {"weapon": weapon, "property": property}
+    console.log(item)
+    return item
+  }
+
+  render() {
+    let item = this.generateItem();
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Magic item generator</h1>
         </header>
-        <div class="Item-info">
-          <h2 className="App-intro">{item}
-          </h2>
-          <p className="App-description">{property}</p>
-        </div>
+        <button onClick={(e) => this.updateList(item)}>Another!</button>
+        {this.state.generated.map(item => (
+          <div className="Item-info">
+            <h2 className="App-intro">{item.weapon}
+            </h2>
+            <p className="App-description">{item.property}</p>
+          </div>
+        ))}
+        <button onClick={(e) => this.updateList(item)}>Another!</button>
       </div>
     );
   }
