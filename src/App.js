31dae4027391d297ee50620e76b9125d8c67cf5f
Augusta MacDonald <optissimum@gmail.com>
2020-04-07 19:13:37 -0400

    Updated CSS and added more magical effects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import Lists from './magic-properties.json';
+
 
 class App extends Component {
   constructor(props) {
@@ -16,13 +18,12 @@ class App extends Component {
   }
 
   updateList(item){
-    console.log(item);
     this.setState({generated: [...this.state.generated, {"weapon": item.weapon, "property": item.property}]});
-    console.log(this.state);
   }
 
   generateItem(){
     let item = {}
+    let distance = Lists.distance[this.random(Lists.distance.length)]
     let property = Lists
       .property[this.random(Lists.property.length)]
       .replace("#trigger", Lists.trigger[this.random(Lists.trigger.length)])
@@ -32,15 +33,14 @@ class App extends Component {
       .replace("#damage_type", Lists.damage_type[this.random(Lists.damage_type.length)])
       .replace("#action", Lists.action[this.random(Lists.action.length)])
       .replace("#save", Lists.save[this.random(Lists.save.length)])
-      .replace("#distance", Lists.distance[this.random(Lists.distance.length)] + "ft")
-      .replace("#distance", Math.min([Lists.distance[this.random(Lists.distance.length)], Lists.distance[this.random(Lists.distance.length)]]) + "ft")
+      .replace("#distance", distance + "ft")
+      .replace("#distance", Math.min(distance,  Lists.distance[this.random(Lists.distance.length)]) + "ft")
       .replace("#duration", Lists.duration[this.random(Lists.duration.length)]);
     property = property
       .charAt(0)
       .toUpperCase() + property.substr(1);
     let weapon = Lists.weapons[this.random(Lists.weapons.length)];
     item = {"weapon": weapon, "property": property}
-    console.log(item)
     return item
   }
 
@@ -52,15 +52,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Magic item generator</h1>
         </header>
-        <button onClick={(e) => this.updateList(item)}>Another!</button>
         {this.state.generated.map(item => (
           <div className="Item-info">
-            <h2 className="App-intro">{item.weapon}
-            </h2>
+            <h3 className="App-intro">{item.weapon}</h3>
             <p className="App-description">{item.property}</p>
           </div>
         ))}
-        <button onClick={(e) => this.updateList(item)}>Another!</button>
+        <a className="App-button" onClick={(e) => this.updateList(item)}> {this.state.generated.length == 0 ? "Generate Weapon" : "Another!"} </a>
       </div>
     );
   }
